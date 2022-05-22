7ebf6a5306111f3728e7ff9cca1ea51694d5fb29
rvdemael <romain.v@live.be>
2022-05-22 21:40:51 +0200

    correction date supprimée par erreur +

--- src/CalendarICS.java
+++ src/CalendarICS.java
@@ -1,13 +1,9 @@
-
-
 import java.io.*;
 import java.util.Scanner;
 import java.util.UUID;
 
 
 public class CalendarICS {
-
-
 
     static String prodid =     "PRODID:-//Romain//FormationDevAndroid//\r\n";
     static String calBegin =   "BEGIN:VCALENDAR\r\n";
@@ -24,12 +20,11 @@ public class CalendarICS {
         int index1 = eventLine.indexOf("\"");
         int index2 = eventLine.lastIndexOf("\"");
 
-        tokens[0] = eventLine.substring(0,index);
-        tokens[1] = eventLine.substring(index+1,index+2);
-        tokens[2] = eventLine.substring(index1+1,index2);
-        tokens[3] = eventLine.substring(n-4,n-2);
-        tokens[4] = eventLine.substring(n-1);
-        System.out.println(tokens[0]+" "+tokens[1]+" "+tokens[2]+" "+tokens[3]+" "+tokens[4]);
+        tokens[0] = eventLine.substring(0,index); //Date yyyymmdd
+        tokens[1] = eventLine.substring(index+1,index+2); //C or T
+        tokens[2] = eventLine.substring(index1+1,index2);//Title
+        tokens[3] = eventLine.substring(n-4,n-2); //AM,PM or AD (all day)
+        tokens[4] = eventLine.substring(n-1); //D or P at home or at the office
 
         String event = eventBegin;
         event += "DTSTART:"  + tokens[0]  + "T";
@@ -48,13 +43,14 @@ public class CalendarICS {
         event += "DTSTAMP:"   + "20220516" + "T" + "060000Z" + "\r\n";
         UUID uuid = UUID.randomUUID();
         event += "UID:" + uuid.toString() + "\r\n" ;
-        event += "DESCRIPTION:" + "\r\n";
+
 
         if(tokens[1].equals("C") ) {
             event += "SUMMARY:Cefora : "+ tokens[2] + "\r\n";
-            event += "DESCRIPTION:" + tokens[2] + "\r\n";
+            event += "DESCRIPTION:" + tokens[2] + ", Local 3.2\r\n";
         }else {
             event += "SUMMARY:"+tokens[2] + "\r\n";
+            event += "DESCRIPTION: Local 2.3" + "\r\n";
         }
 
         if (tokens[0].strip().equals("20220902")) {
@@ -72,9 +68,7 @@ public class CalendarICS {
 
     public static void calendarMaker(String filename)  {
 
-
         String intro = calBegin + prodid + version + "CALSCALE:GREGORIAN\r\n" + "METHOD:PUBLISH\r\n" + "X-WR-CALNAME:Dev android\r\n" + "X-WR-TIMEZONE:Europe/Brussels\r\n";
-
         File myFile = new File(filename);
         String events ="";
         try {
@@ -102,6 +96,10 @@ public class CalendarICS {
             e.printStackTrace();
         }
 
+    }
+
+    public static void main(String[] args) {
+        calendarMaker("src/schedule.txt");
     }
 
 
